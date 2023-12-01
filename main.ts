a2a9a69e39148c868be0779e91c66df0fe9f3dad
charles20120806 <152579459+charles20120806@users.noreply.github.com>
2023-12-01 11:49:13 +0800

    gfgfgfgf

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
 input.onButtonPressed(Button.A, function () {
     if (input.compassHeading() > 45 && input.compassHeading() < 135) {
-    	
+        basic.showString("E")
+    } else if (input.compassHeading() > 135 && input.compassHeading() < 225) {
+        basic.showString("S")
+    } else if (input.compassHeading() > 225 && input.compassHeading() < 315) {
+        basic.showString("W")
+    } else if (input.compassHeading() > 315 && input.compassHeading() < 45) {
+        basic.showString("N")
     } else {
-    	
+        basic.showString("X")
     }
+    basic.pause(1000)
+    basic.clearScreen()
 })
